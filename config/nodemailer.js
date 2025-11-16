85217303e5d156243794aac3be1d6463213537f3
Ismailirshad <irshadsha164@gmail.com>
2025-11-16 11:55:20 +0530

    nodemailer fixed

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,12 +1,20 @@
 import nodemailer from 'nodemailer';
 
+// const transporter = nodemailer.createTransport({
+//    service:'gmail',
+//     auth: {
+//         user:process.env.SMTP_USER,
+//         pass:process.env.SMTP_PASS,
+//     }
+// })
 const transporter = nodemailer.createTransport({
-   service:'gmail',
-    auth: {
-        user:process.env.SMTP_USER,
-        pass:process.env.SMTP_PASS,
-    }
-})
+  host: 'smtp.sendgrid.net',
+  port: 587,
+  auth: {
+    user: 'apikey', // literally the word 'apikey'
+    pass: process.env.SENDGRID_API_KEY, // your SendGrid API key
+  },
+});
 
 transporter.verify(function(err, success) {
     if (err) console.log('SMTP Verification Error:', err);
