589ebe089f98ae5b09709c379a373e2770cb16ab
Ismailirshad <irshadsha164@gmail.com>
2025-11-16 11:12:04 +0530

    nodemailer fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/User.js';
 import { EMAIL_VERIFY_TEMPLATE } from '../config/emailTemplate.js';
+import transporter from '../config/nodemailer.js';
 // import { Resend } from 'resend';
 // const resend = new Resend(process.env.RESEND_API_KEY);
 
