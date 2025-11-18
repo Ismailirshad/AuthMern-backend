d941d85928b52a67399e2ab8662122227651f113
Ismailirshad <irshadsha164@gmail.com>
2025-11-18 14:40:43 +0530

    All bugs fixed

--- config/emailVerifyTemplate.js
+++ config/emailVerifyTemplate.js
@@ -1,118 +1,72 @@
 export const VERIFY_EMAIL_TEMPLATE = `
-<td bgcolor="#fafafa" align="center" class="esd-stripe" style="background-color:rgb(250, 250, 250)">
-  <table esd-img-prev-src width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" class="es-content-body" style="background-color:rgb(255, 255, 255)">
+<td bgcolor="#fafafa" align="center" class="esd-stripe" style="background-color:#fafafa">
+  <table width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" class="es-content-body" style="background-color:#ffffff">
     <tbody>
+
       <tr>
-        <td esd-img-prev-src="https://fkus.stripocdn.email/content/guids/CABINET_8a8240f4650bd716d3cd69675fe184ca/images/1041555765740937.png" esd-img-prev-position="left top" esd-img-prev-repeat="no-repeat" bgcolor="transparent" align="left" class="esd-structure es-p40t es-p20r es-p20l" style="background-color:transparent;background-position:left top">
+        <td align="center" class="esd-structure es-p40t es-p20r es-p20l">
+
           <table width="100%" cellspacing="0" cellpadding="0">
             <tbody>
               <tr>
                 <td width="560" valign="top" align="center" class="esd-container-frame">
-                  <table esd-img-prev-src esd-img-prev-position="left top" width="100%" cellspacing="0" cellpadding="0" style="background-position:left top">
+
+                  <table width="100%" cellspacing="0" cellpadding="0">
+
                     <tbody>
+
+                      <!-- Logo -->
                       <tr>
-                        <td align="center" class="esd-block-image es-p5t es-p5b" style="font-size:0">
-                          <a target="_blank">
-                            <img src="https://fwaosgn.stripocdn.email/content/guids/CABINET_dd354a98a803b60e2f0411e893c82f56/images/23891556799905703.png" alt="" width="175" style="display:block">
-                          </a>
+                        <td align="center" class="esd-block-image es-p10t es-p10b">
+                          <img src="https://fwaosgn.stripocdn.email/content/guids/CABINET_dd354a98a803b60e2f0411e893c82f56/images/23891556799905703.png" alt="" width="175" style="display:block">
                         </td>
                       </tr>
 
+                      <!-- Heading -->
                       <tr>
-                        <td align="center" class="esd-block-text es-p15t es-p15b">
-                          <h1 style="color:#333333;font-size:20px"><strong>FORGOT YOUR</strong></h1>
-                          <h1 style="color:#333333;font-size:20px"><strong>PASSWORD?</strong></h1>
+                        <td align="center" class="esd-block-text es-p10t es-p10b">
+                          <h1 style="color:#333;font-size:22px;margin:0"><strong>VERIFY YOUR EMAIL</strong></h1>
                         </td>
                       </tr>
 
+                      <!-- Greeting -->
                       <tr>
-                        <td align="left" class="esd-block-text es-p40r es-p40l">
-                          <p style="text-align:center">HI, {{email}}</p>
+                        <td align="center" class="esd-block-text es-p10b">
+                          <p style="font-size:16px;margin:0">Hi, <strong>{{email}}</strong></p>
                         </td>
                       </tr>
 
+                      <!-- Description -->
                       <tr>
-                        <td align="left" class="esd-block-text es-p35r es-p40l">
-                          <p style="text-align:center">There was a request to change your password!</p>
+                        <td align="center" class="esd-block-text es-p10t es-p10b">
+                          <p style="font-size:15px;margin:0">
+                            Thank you for registering! Please use the OTP below to verify your email address:
+                          </p>
                         </td>
                       </tr>
 
+                      <!-- OTP BOX -->
                       <tr>
-                        <td align="center" class="esd-block-text es-p25t es-p40r es-p40l">
-                          <p>If you did not make this request, just ignore this email. Otherwise, please use the OTP below to change your password:</p>
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td align="center" class="esd-block-button es-p40t es-p40b es-p10r es-p10l">
-                          <span style="font-size:24px; font-weight:bold; padding:12px 20px; display:inline-block; background:#eeeeee; border-radius:8px;">
+                        <td align="center" class="esd-block-button es-p30t es-p30b">
+                          <span style="font-size:26px;font-weight:bold;padding:14px 22px; background:#eeeeee; border-radius:10px; display:inline-block;">
                             {{otp}}
                           </span>
                         </td>
                       </tr>
 
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      </tr>
-
-      <tr>
-        <td align="left" class="esd-structure es-p20t es-p10r es-p10l">
-          <table cellspacing="0" cellpadding="0" align="left" class="es-left">
-            <tbody>
-              <tr>
-                <td width="199" align="left" class="esd-container-frame">
-                  <table width="100%" cellspacing="0" cellpadding="0">
-                    <tbody>
+                      <!-- Note -->
                       <tr>
-                        <td align="right" class="esd-block-text es-p15t">
-                          <p style="font-size:16px;color:#666666"><strong>Follow us:</strong></p>
+                        <td align="center" class="esd-block-text es-p10t es-p10b">
+                          <p style="font-size:14px;margin:0;color:#555">
+                            If you did not request this email, you can safely ignore it.
+                          </p>
                         </td>
                       </tr>
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            </tbody>
-          </table>
 
-          <table cellspacing="0" cellpadding="0" align="right" class="es-right">
-            <tbody>
-              <tr>
-                <td width="361" align="left" class="esd-container-frame">
-                  <table width="100%" cellspacing="0" cellpadding="0">
-                    <tbody>
-                      <tr>
-                        <td align="left" class="esd-block-social es-p10t es-p5b" style="font-size:0">
-                          <table cellspacing="0" cellpadding="0" class="es-table-not-adapt es-social">
-                            <tbody>
-                              <tr>
-                                <td align="center" class="es-p10r">
-                                  <a target="_blank" href=""><img src="https://fwaosgn.stripocdn.email/content/assets/img/social-icons/rounded-gray/facebook-rounded-gray.png" width="32"></a>
-                                </td>
-                                <td align="center" class="es-p10r">
-                                  <a target="_blank" href=""><img src="https://fwaosgn.stripocdn.email/content/assets/img/social-icons/rounded-gray/x-rounded-gray.png" width="32"></a>
-                                </td>
-                                <td align="center" class="es-p10r">
-                                  <a target="_blank" href=""><img src="https://fwaosgn.stripocdn.email/content/assets/img/social-icons/rounded-gray/instagram-rounded-gray.png" width="32"></a>
-                                </td>
-                                <td align="center" class="es-p10r">
-                                  <a target="_blank" href=""><img src="https://fwaosgn.stripocdn.email/content/assets/img/social-icons/rounded-gray/youtube-rounded-gray.png" width="32"></a>
-                                </td>
-                                <td align="center" class="es-p10r">
-                                  <a target="_blank" href=""><img src="https://fwaosgn.stripocdn.email/content/assets/img/social-icons/rounded-gray/linkedin-rounded-gray.png" width="32"></a>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
                     </tbody>
+
                   </table>
+
                 </td>
               </tr>
             </tbody>
@@ -121,21 +75,14 @@ export const VERIFY_EMAIL_TEMPLATE = `
         </td>
       </tr>
 
+      <!-- Footer -->
       <tr>
-        <td align="left" class="esd-structure es-p5t es-p20b es-p20r es-p20l">
+        <td align="center" class="esd-structure es-p20t es-p20b es-p20r es-p20l">
           <table width="100%" cellspacing="0" cellpadding="0">
             <tbody>
               <tr>
-                <td width="560" valign="top" align="center" class="esd-container-frame">
-                  <table width="100%" cellspacing="0" cellpadding="0">
-                    <tbody>
-                      <tr>
-                        <td align="center" class="esd-block-text">
-                          <p style="font-size:14px">Contact us: irshadsha164@gmail.com</p>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                <td align="center" class="esd-block-text">
+                  <p style="font-size:14px;color:#666;margin:0">Contact us: irshadsha164@gmail.com</p>
                 </td>
               </tr>
             </tbody>
